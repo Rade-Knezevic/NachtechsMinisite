2ceee9e469bd96f4903c75455bd63851822d640c
Rade <rade@picotrip.com>
2025-07-29 15:56:54 +0200

    first commit

---
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => "successfully logged, {$user->username}"
+            ]);
+        }
+
+        return response()->json([
+            'message' => "invalid credentials"
+        ], 401);
+    }
+}
